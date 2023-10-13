52b28387ff3b8af9cd9b58659616dedc3cd51922
Brady002 <bradywright@u.boisestate.edu>
2023-10-13 17:53:29 -0600

    Messed with splines

--- Inertia Override/Assets/Camera Controller.cs
+++ Inertia Override/Assets/Camera Controller.cs
@@ -10,14 +10,20 @@ public class CameraController : MonoBehaviour
     private float mouseY;
     public Vector2 mouse;
     private float horizontalInput;
+    public GameObject c;
+    public GameObject centerpoint;
+
     public float sensitivityX = 1000f;
     public float sensitivityY = 1000f;
+
+    private bool locked = true;
 
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        StartCoroutine(CameraLockRelease());
     }
 
     public void OnLook(InputAction.CallbackContext context)
@@ -27,12 +33,24 @@ public class CameraController : MonoBehaviour
 
     private void Update()
     {
-        mouseX = mouse.x * Time.deltaTime * sensitivityX;
-        mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensitivityY;
-        horizontalInput = Input.GetAxis("Horizontal") * Time.deltaTime * sensitivityX;
+        if(!locked)
+        {
+            mouseX = mouse.x * Time.deltaTime * sensitivityX;
+            mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensitivityY;
+            horizontalInput = Input.GetAxis("Horizontal") * Time.deltaTime * sensitivityX;
 
-        transform.Rotate(Vector3.up, mouseX);
-        //transform.Rotate(Vector3.up, horizontalInput);
+            transform.Rotate(Vector3.up, mouseX);
+            //transform.Rotate(Vector3.up, horizontalInput);
+        }
+
+    }
+
+    private IEnumerator CameraLockRelease()
+    {
+        yield return new WaitForSeconds(20);
+        c.transform.position = centerpoint.transform.position;
+        c.transform.rotation = centerpoint.transform.rotation;
+        locked = false;
     }
 
 }
